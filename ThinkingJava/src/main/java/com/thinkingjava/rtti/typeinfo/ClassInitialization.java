204ca9dcbc5ba6643936790af51d7e1fd9aab93f
wang wei <borderTraveler.wang@gmail.com>
2014-06-28 22:50:46 +0800

    new add  code

--- ThinkingJava/src/main/java/com/thinkingjava/rtti/typeinfo/ClassInitialization.java
+++ ThinkingJava/src/main/java/com/thinkingjava/rtti/typeinfo/ClassInitialization.java
@@ -7,14 +7,15 @@ public class ClassInitialization {
 	public static Random rand = new Random(47);
 	public static void main(String[] args) throws ClassNotFoundException{
 		Class initable = Initable.class;
-		System.out.println("After creating Initable ref");
-		System.out.println(Initable.staticFinal);
-		System.out.println(Initable.staticFinal2);
-		
+//		System.out.println("After creating Initable ref");
+//		System.out.println(Initable.staticFinal);
+//		System.out.println(Initable.staticFinal2);
+//		
 		System.out.println(Initable2.staticNonFinal);
+//		
+//		Class  initable3 = Class.forName("com.thinkingjava.rtti.typeinfo.Initable3");
 		
-		Class  initable3 = Class.forName("com.thinkingjava.rtti.typeinfo.Initable3");
-		System.out.println(Initable3.staticNonFinal);
+//		System.out.println(Initable3.staticNonFinal);
 	}
 }
 
