9009dff3c09a0bff500c9a08e0ef5d5f9bda02fa
wang wei <borderTraveler.wang@gmail.com>
2014-06-29 21:24:46 +0800

    wildcard

---
+++ ThinkingJava/src/main/java/com/thinkingjava/rtti/typeinfo/BoundedClassReferences.java
@@ -0,0 +1,16 @@
+package com.thinkingjava.rtti.typeinfo;
+
+/**
+ * 
+ * @author wangwei
+ *
+ */
+public class BoundedClassReferences {
+	
+	public static void main(String[] args){
+		
+		Class<? extends Number> bounded = int.class;
+		bounded = double.class;
+		bounded = Number.class;
+	}
+}
---
+++ ThinkingJava/src/main/java/com/thinkingjava/rtti/typeinfo/GenericClassReferences.java
@@ -0,0 +1,17 @@
+package com.thinkingjava.rtti.typeinfo;
+
+public class GenericClassReferences {
+	
+	public static void main(String[] args){
+		Class intClass = int.class;
+		Class<Integer> genericIntClass = int.class;
+		genericIntClass = Integer.class;
+		
+		intClass = double.class;
+		
+//		genericIntClass= double.class;
+		
+//		Class<Number> genericNumberClass = int.class;
+		// Integer class 对象不是Integer class的子对象
+	}
+}
