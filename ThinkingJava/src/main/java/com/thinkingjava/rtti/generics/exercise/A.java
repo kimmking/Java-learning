94d54c4eda9f095a7850b7aae020e677ff568fc6
wang wei <borderTraveler.wang@gmail.com>
2014-11-28 00:51:22 +0800

    new add

---
+++ ThinkingJava/src/main/java/com/thinkingjava/rtti/generics/exercise/A.java
@@ -0,0 +1,9 @@
+package com.thinkingjava.rtti.generics.exercise;
+
+
+public class A {
+	
+	public static void main(String[] args){
+		
+	}
+}
---
+++ ThinkingJava/src/main/java/com/thinkingjava/rtti/generics/exercise/ErasedTypeInter.java
@@ -0,0 +1,9 @@
+package com.thinkingjava.rtti.generics.exercise;
+
+public interface ErasedTypeInter {
+	
+	public void test1();
+	
+	public void test2();
+	
+}
