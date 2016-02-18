ab574b4b30464f397cc5267ab412272378b8565b
王维 <bordertraveler.wang@gmail.com>
2016-02-19 01:31:29 +0800

    add

---
+++ ThinkingJava/src/main/java/com/thinkingjava/concurrent/thread/ThreadMain.java
@@ -0,0 +1,25 @@
+package com.thinkingjava.concurrent.thread;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.FutureTask;
+
+/**
+ * Created by wangwei on 16/2/19.
+ */
+public class ThreadMain {
+
+    public static void main(String[] args) {
+        ThreadA threadA = new ThreadA();
+        FutureTask<String> futureTask = new FutureTask<String>(threadA);
+        new Thread(futureTask).start();
+        System.out.println("这是主线程:begin!");
+        try {
+            System.out.println("得到的返回结果是:" + futureTask.get());
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
---
+++ wangwei/src/main/java/com/borderTraveler/wangwei/App.java
@@ -0,0 +1,13 @@
+package com.borderTraveler.wangwei;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
