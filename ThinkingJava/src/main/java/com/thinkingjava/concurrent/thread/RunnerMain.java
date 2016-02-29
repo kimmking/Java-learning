cc6826d30776cc300a2b7e89a270049f6999e3b8
王维 <bordertraveler.wang@gmail.com>
2016-02-29 22:15:04 +0800

    验证 start 与 run 的区别

---
+++ ThinkingJava/src/main/java/com/thinkingjava/concurrent/thread/RunnerMain.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2016. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+ * Morbi non lorem porttitor neque feugiat blandit. Ut vitae ipsum eget quam lacinia accumsan.
+ * Etiam sed turpis ac ipsum condimentum fringilla. Maecenas magna.
+ * Proin dapibus sapien vel ante. Aliquam erat volutpat. Pellentesque sagittis ligula eget metus.
+ * Vestibulum commodo. Ut rhoncus gravida arcu.
+ */
+
+package com.thinkingjava.concurrent.thread;
+
+/**
+ * Created by wangwei on 16/2/29.
+ */
+public class RunnerMain {
+
+    public static void main(String[] args) {
+
+        Runner1 runner1 = new Runner1();
+        Thread thread1 = new Thread(runner1);
+
+        Runner2 runner2 = new Runner2();
+        Thread thread2 = new Thread(runner2);
+
+        thread1.start();
+        thread2.start();
+
+        System.out.println(Thread.activeCount());
+    }
+}
