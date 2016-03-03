f0f109fbc756d2e0c6c61c023953d7544941f9f0
王维 <bordertraveler.wang@gmail.com>
2016-03-03 23:43:31 +0800

    thread exception catch

---
+++ ThinkingJava/src/main/java/com/thinkingjava/concurrent/thread/exceptioncaught/ExceptionHandlerThreadB.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2016. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+ * Morbi non lorem porttitor neque feugiat blandit. Ut vitae ipsum eget quam lacinia accumsan.
+ * Etiam sed turpis ac ipsum condimentum fringilla. Maecenas magna.
+ * Proin dapibus sapien vel ante. Aliquam erat volutpat. Pellentesque sagittis ligula eget metus.
+ * Vestibulum commodo. Ut rhoncus gravida arcu.
+ */
+
+package com.thinkingjava.concurrent.thread.exceptioncaught;
+
+/**
+ * Created by wangwei on 16/3/3.
+ */
+public class ExceptionHandlerThreadB implements Thread.UncaughtExceptionHandler {
+    /**
+     * Method invoked when the given thread terminates due to the
+     * given uncaught exception.
+     * <p>Any exception thrown by this method will be ignored by the
+     * Java Virtual Machine.
+     *
+     * @param t the thread
+     * @param e the exception
+     */
+    @Override
+    public void uncaughtException(Thread t, Throwable e) {
+        System.out.printf("An exception has bean captured\n");
+        System.out.printf("Thread: %s\n", t.getId());
+        System.out.printf("Exception: %s: %s\n", e.getClass().getName(), e.getMessage());
+        System.out.printf("Stack Trace: \n");
+        e.printStackTrace(System.out);
+        System.out.printf("Thread status: %s\n", t.getState());
+    }
+}
