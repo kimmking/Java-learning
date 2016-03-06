02e8bdb14d1444902ee26e0e54674f345099f768
王维 <bordertraveler.wang@gmail.com>
2016-03-06 19:43:20 +0800

    thread lock

---
+++ ThinkingJava/src/main/java/com/thinkingjava/concurrent/thread/lock/Count.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright (c) 2016. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+ * Morbi non lorem porttitor neque feugiat blandit. Ut vitae ipsum eget quam lacinia accumsan.
+ * Etiam sed turpis ac ipsum condimentum fringilla. Maecenas magna.
+ * Proin dapibus sapien vel ante. Aliquam erat volutpat. Pellentesque sagittis ligula eget metus.
+ * Vestibulum commodo. Ut rhoncus gravida arcu.
+ */
+
+package com.thinkingjava.concurrent.thread.lock;
+
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * Created by wangwei on 16/3/6.
+ */
+public class Count {
+
+    final ReentrantLock lock = new ReentrantLock();
+
+    public void get() {
+//        final ReentrantLock lock = new ReentrantLock();
+        try {
+            lock.lock();
+            System.out.println(Thread.currentThread().getName() + " get begin");
+            Thread.sleep(1000);
+            System.out.println(Thread.currentThread().getName() + " get end");
+            lock.unlock();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void put() {
+
+        try {
+            lock.lock();
+            System.out.println(Thread.currentThread().getName() + " put begin");
+            Thread.sleep(1000);
+            System.out.println(Thread.currentThread().getName() + " put end");
+            lock.unlock();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ ThinkingJava/src/main/java/com/thinkingjava/concurrent/thread/lock/ReentrantLockDemo.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) 2016. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+ * Morbi non lorem porttitor neque feugiat blandit. Ut vitae ipsum eget quam lacinia accumsan.
+ * Etiam sed turpis ac ipsum condimentum fringilla. Maecenas magna.
+ * Proin dapibus sapien vel ante. Aliquam erat volutpat. Pellentesque sagittis ligula eget metus.
+ * Vestibulum commodo. Ut rhoncus gravida arcu.
+ */
+
+package com.thinkingjava.concurrent.thread.lock;
+
+/**
+ * Created by wangwei on 16/3/6.
+ */
+public class ReentrantLockDemo {
+
+    public static void main(String[] args) {
+        final Count count = new Count();
+
+        for (int i = 0; i < 2; i++) {
+            new Thread() {
+                public void run() {
+                    count.get();
+                }
+            }.start();
+        }
+
+        for (int i = 0; i < 2; i++) {
+            new Thread() {
+                public void run() {
+                    count.put();
+                }
+            }.start();
+        }
+    }
+}
