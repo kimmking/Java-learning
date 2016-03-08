4d08e1f3e447fd0fa03e9cf3df14d78ccd8fa2ce
王维 <bordertraveler.wang@gmail.com>
2016-03-08 20:45:44 +0800

    ReentrantReadWriteLock

--- ThinkingJava/src/main/java/com/thinkingjava/concurrent/thread/lock/Count.java
+++ ThinkingJava/src/main/java/com/thinkingjava/concurrent/thread/lock/Count.java
@@ -15,10 +15,10 @@ import java.util.concurrent.locks.ReentrantLock;
  */
 public class Count {
 
-//    final ReentrantLock lock = new ReentrantLock();
+    final ReentrantLock lock = new ReentrantLock();
 
     public void get() {
-        final ReentrantLock lock = new ReentrantLock();
+//        final ReentrantLock lock = new ReentrantLock();
         try {
             lock.lock();
             System.out.println(Thread.currentThread().getName() + " get begin");
@@ -31,7 +31,7 @@ public class Count {
     }
 
     public void put() {
-        final ReentrantLock lock = new ReentrantLock();
+//        final ReentrantLock lock = new ReentrantLock();
         try {
             lock.lock();
             System.out.println(Thread.currentThread().getName() + " put begin");
