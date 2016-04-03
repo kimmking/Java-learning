cba67091a328baacb68dd15853448f186993fbf3
王维 <bordertraveler.wang@gmail.com>
2016-04-03 18:41:08 +0800

    NewCachedThreadPoolDemo

---
+++ ThinkingJava/src/main/java/com/thinkingjava/concurrent/thread/threadpool/NewCachedThreadPoolDemo.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright (c) 2016. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+ * Morbi non lorem porttitor neque feugiat blandit. Ut vitae ipsum eget quam lacinia accumsan.
+ * Etiam sed turpis ac ipsum condimentum fringilla. Maecenas magna.
+ * Proin dapibus sapien vel ante. Aliquam erat volutpat. Pellentesque sagittis ligula eget metus.
+ * Vestibulum commodo. Ut rhoncus gravida arcu.
+ */
+
+package com.thinkingjava.concurrent.thread.threadpool;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * Created by wangwei on 16/4/3.
+ */
+public class NewCachedThreadPoolDemo {
+
+    public static void main(String[] args) {
+
+        ExecutorService executorService = Executors.newCachedThreadPool();
+
+        for (int i = 0; i < 100; i++) {
+            final int no = i;
+            Runnable runnable = new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        System.out.println("start:" + no);
+                        Thread.sleep(1000L);
+                        System.out.println("end:" + no);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            };
+            executorService.execute(runnable);
+        }
+        executorService.shutdown();
+        System.out.println("Main Thread End!");
+
+
+    }
+
+}
