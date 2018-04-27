3c552ec40b7ba474d3b8e511458519b0184dce16
wangwei <w7years@gmail.com>
2018-04-27 17:52:50 +0800

    impl myArrayList

---
+++ backend-algorithms/src/main/java/one/wangwei/algorithms/datastructures/list/interfaces/IList.java
@@ -0,0 +1,64 @@
+package one.wangwei.algorithms.datastructures.list.interfaces;
+
+/**
+ * 线性表接口
+ *
+ * @param <T>
+ * @author wangwei
+ * @date 2018/04/27
+ */
+public interface IList<T> {
+
+    /**
+     * 添加元素
+     *
+     * @param element
+     * @return
+     */
+    public boolean add(T element);
+
+    /**
+     * 移除元素
+     *
+     * @param element
+     * @return
+     */
+    public boolean remove(T element);
+
+    /**
+     * 删除 index 位置上的元素
+     *
+     * @param index
+     * @return
+     */
+    public T remove(int index);
+
+    /**
+     * 设置index上的元素
+     *
+     * @param index
+     * @param element
+     * @return
+     */
+    public T set(int index, T element);
+
+    /**
+     * 清空list集合
+     */
+    public void clear();
+
+    /**
+     * 判断是否包含某个元素
+     *
+     * @param element
+     */
+    public boolean contains(T element);
+
+    /**
+     * 集合大小
+     *
+     * @return
+     */
+    public int size();
+
+}
---
+++ backend-algorithms/src/test/java/one/wangwei/algorithms/datastructures/list/impl/MyArrayListTest.java
@@ -0,0 +1,15 @@
+package one.wangwei.algorithms.datastructures.list.impl;
+
+public class MyArrayListTest {
+
+    public static void main(String[] args) {
+        MyArrayList<Integer> list = new MyArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            list.add(i);
+        }
+        list.remove(9);
+        list.add(null);
+        list.clear();
+        System.out.println(list);
+    }
+}
